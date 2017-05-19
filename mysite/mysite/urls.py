7b92bfbdc2d353bb8b382d19807637c0134977cd
Zoe <zkendall@smith.edu>
2017-05-19 13:51:50 -0400

    added Gantt homepage

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import main.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', main.views.gantt, name='gantt')
 ]
