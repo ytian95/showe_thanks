fea9efa7a375664a6a67c614ab175548fff9adfe
Youyou Tian <ytian95@gmail.com>
2017-05-24 20:37:39 -0400

    Created preliminary model

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+class Profile(models.Model):
+    DC_TEAMS = (
+        ("DR", "Dresser-Rand").
+        ("HF", "HeartFlow"),
+        ("IL", "Instrumentation Laboratory"),
+        ("MD", "Medtronic"),
+        ("NOPS", "Northampton OPS"),
+        ("ST", "Stantec"),
+        ("SR", "Sterling Rope")
+    )
+    name = models.CharField(max_length=256) # Ask for both first + last
+    DC_team = models.CharField(max_length=4, choices=DC_TEAMS) # change to dropdown
+    work_location_name = models.CharField(max_length=128)
+    work_city = models.CharField(max_length=128)
+    work_state = models.CharField(max_length=128)
+    work_country = models.CharField(max_length=128)
+    start_date = models.DateField("Start date")
+    email = models.EmailField(max_length=128)
+    profile_pic = models.ImageField(upload_to='img/profile/%Y/%m/')
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
 # Create your models here.
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = '/media/'
+MEDIA_URL = ''
